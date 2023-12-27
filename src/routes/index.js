c1a877b0c7541450672bc220e83f585e8a6b60c4
JoseAlz <joseufox@hotmail.com>
2023-12-27 12:25:11 -0300

    primera web node

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+import { Router } from "express"
+
+const router = Router();
+
+router.get('/', (req, res) => {
+res.render('index', {title: "First Web Node"});
+});
+
+router.get('/about', (req, res) => {
+res.render('about',{ title: "About First Node website"});
+});
+
+router.get('/contact', (req, res) => {
+res.render('contact',{ title: "Contact page"});
+});
+
+export default router;
